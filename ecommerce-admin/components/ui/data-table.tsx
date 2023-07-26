5db8dc3bb0af9db43cc50597b290dc77cb60a600
Ken Ratchapon <ratchapon.pk@outlook.com>
2023-07-26 17:01:32 +0700

    categories entitty create new prisma db model for Category https://ui.shadcn.com/docs/components/select clone billboard folder to category

--- ecommerce-admin/components/ui/data-table.tsx
+++ ecommerce-admin/components/ui/data-table.tsx
@@ -52,9 +52,9 @@ export function DataTable<TData, TValue>({
           <div className="flex items-center py-4">
             <Input
               placeholder="Search"
-              value={(table.getColumn("searchKey")?.getFilterValue() as string) ?? ""}
+              value={(table.getColumn(`${searchKey}`)?.getFilterValue() as string) ?? ""}
               onChange={(event) =>
-                table.getColumn("searchKey")?.setFilterValue(event.target.value)
+                table.getColumn(`${searchKey}`)?.setFilterValue(event.target.value)
               }
               className="max-w-sm"
             />
